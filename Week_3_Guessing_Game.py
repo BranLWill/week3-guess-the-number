e3a297e5c4de09c05d8de9ce905e0cc794c63a3c
Brandon Willett <bwillett@mail.valenciacollege.edu>
2016-02-04 11:01:00 -0500

    Update Week_3_Guessing_Game.py

--- Week_3_Guessing_Game.py
+++ Week_3_Guessing_Game.py
@@ -105,8 +105,8 @@ def playGame( showAnswer ):
     # you don't need to change anything below this comment ##############
     # ///////////////////////////////////////////////////////////////////
     # this if statement allows us to show the hidden number to the user
-    if( showAnswer == True ):
-        print('--shhh, the real number is ' + str(theNumber) + '.')
+if( showAnswer == True ):
+    print('--shhh, the real number is ' + str(theNumber) + '.')
     
     #this gives a sucess/fail message if the user guessed correctly in the allotted attempts
     if askUserToGuess(totalGuesses,theNumber) == True:
